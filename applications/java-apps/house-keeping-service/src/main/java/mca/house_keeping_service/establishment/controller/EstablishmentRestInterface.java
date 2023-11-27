ce79b9dfaefd99c611d33b6d15a1d706b2741182
Víctor H. <hugohg34@gmail.com>
2023-11-27 23:58:28 +0100

    OpenApi documentation

--- applications/java-apps/house-keeping-service/src/main/java/mca/house_keeping_service/establishment/controller/EstablishmentRestInterface.java
+++ applications/java-apps/house-keeping-service/src/main/java/mca/house_keeping_service/establishment/controller/EstablishmentRestInterface.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
@@ -22,30 +23,43 @@ import mca.house_keeping_service.establishment.dto.EstablishmentRespDTO;
 @RequestMapping(value = "/api/establishments", produces = MediaType.APPLICATION_JSON_VALUE)
 public interface EstablishmentRestInterface {
 
+	@Operation(summary = "Get a list of all establishments")
+	@ApiResponse(description = "List of establishments", responseCode = "200")
 	@GetMapping
 	public ResponseEntity<Page<EstablishmentRespDTO>> getAllEstablishments(
+			@Parameter(description = "Page number, starts from 0", example = "0")
 			@RequestParam(defaultValue = "0") int page,
+			@Parameter(description = "Number of elements per page", example = "20")
 			@RequestParam(defaultValue = "20") int size,
+			@Parameter(description = "Sorting criteria in the format: property(,asc|desc). "
+					+ "Default sort order is ascending. "
+					+ "Multiple sort criteria are supported.", example = "name,asc")
 			@RequestParam(defaultValue = "id") String sortBy);
 
+	@Operation(summary = "Get a specific establishment")
+	@ApiResponse(description = "Establishment details", responseCode = "200")
 	@GetMapping("/{id}")
 	public ResponseEntity<EstablishmentRespDTO> getEstablishment(
 			@Parameter(description = "Establishment ID", example = "00000000-0000-0000-0000-000000000001")
 			@PathVariable(name = "id") final UUID id);
 
+	@Operation(summary = "Create a new establishment")
+	@ApiResponse(description = "ID establishment", responseCode = "201")
 	@PostMapping
-	@ApiResponse(responseCode = "201")
 	public ResponseEntity<UUID> createEstablishment(
 			@RequestBody @Valid final EstablishmentRespDTO establishmentDTO);
 
+	@Operation(summary = "Update an existing establishment")
+	@ApiResponse(description = "ID establishment", responseCode = "200")
 	@PutMapping("/{id}")
 	public ResponseEntity<UUID> updateEstablishment(
 			@Parameter(description = "Establishment ID", example = "00000000-0000-0000-0000-000000000001")
 			@PathVariable(name = "id") final UUID id,
 			@RequestBody @Valid final EstablishmentRespDTO establishmentDTO);
 
-	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete an existing establishment")
 	@ApiResponse(responseCode = "204")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteEstablishment(
 			@Parameter(description = "Establishment ID", example = "00000000-0000-0000-0000-000000000001")
 			@PathVariable(name = "id") final UUID id);
--- applications/java-apps/house-keeping-service/src/main/java/mca/house_keeping_service/reservation/controller/ReservationRestInterface.java
+++ applications/java-apps/house-keeping-service/src/main/java/mca/house_keeping_service/reservation/controller/ReservationRestInterface.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
@@ -20,15 +21,20 @@ import mca.house_keeping_service.reservation.model.ReservationId;
 @RequestMapping(value = "/api/reservation", produces = MediaType.APPLICATION_JSON_VALUE)
 public interface ReservationRestInterface {
 
+	@Operation(summary = "Get reservation details")
+	@ApiResponse(description = "Reservation details", responseCode = "200")
 	@GetMapping("/{id}")
 	public ResponseEntity<ReservationDTO> getReservation(
 			@Parameter(description = "Unique identifier for the Reservation") @PathVariable(name = "id") final ReservationId resId);
 
+	@Operation(summary = "Create a new reservation")
+	@ApiResponse(description = "ID reservation",  responseCode = "201")
 	@PostMapping
-	@ApiResponse(responseCode = "201")
 	public ResponseEntity<ReservationId> createReservation(
 			@Parameter(description = "Reservation request") @RequestBody @Valid final ReservationReqDTO reservationReqDTO);
 
+	@Operation(summary = "Checkin a reservation")
+	@ApiResponse(description = "ID reservation",  responseCode = "200")	
 	@PutMapping("/{id}/checkin")
 	public ResponseEntity<ReservationId> checkin(
 			@Parameter(description = "Unique identifier for the Reservation") @PathVariable(name = "id") final ReservationId resId,

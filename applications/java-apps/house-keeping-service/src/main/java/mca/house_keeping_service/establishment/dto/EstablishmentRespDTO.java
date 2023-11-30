c4ba46c4cb2cd125a3065956d211da154c8aab99
Víctor H. <hugohg34@gmail.com>
2023-12-01 00:02:33 +0100

    Fixed open api doc name not allow space

--- applications/java-apps/house-keeping-service/src/main/java/mca/house_keeping_service/establishment/dto/EstablishmentRespDTO.java
+++ applications/java-apps/house-keeping-service/src/main/java/mca/house_keeping_service/establishment/dto/EstablishmentRespDTO.java
@@ -2,7 +2,6 @@ package mca.house_keeping_service.establishment.dto;
 
 import java.util.UUID;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -10,7 +9,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Schema(name = "Establishment Resumen")
 public class EstablishmentRespDTO {
 
 	private UUID id;

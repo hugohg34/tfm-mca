c59101e2bd48f6e7d2353a75d5b6da648c33fbc5
Víctor H. <hugohg34@gmail.com>
2023-12-03 23:33:24 +0100

    update test

--- infrastructure/docker/performanceTest/locust/locustfile.py
+++ infrastructure/docker/performanceTest/locust/locustfile.py
@@ -7,11 +7,12 @@ class ApiUser(HttpUser):
 
     def on_start(self):
         self.reservations = self.load_reservations()
-        self.establishment_id = "00000000-0000-0000-0000-000000000000"
+        self.establishment_id = "00000000-0000-0000-0000-000000000001"
         self.room_type_id = self.get_room_type_id()
+
     
     def get_room_type_id(self):
-        response = self.client.get(f"/api/establishments/{self.establishment_id}/rooms/types")     
+        response = self.client.get(f"/api/v1/establishments/{self.establishment_id}/room-types")
         if response.status_code == 200:
             roomsType = json.loads(response.text)
             return [room["id"] for room in roomsType] 
@@ -31,9 +32,10 @@ class ApiUser(HttpUser):
         reservation["roomTypes"][0]["roomTypeId"] = roomType
         reservation["roomTypes"][0]["quantity"] = quantity
         reservation["establishmentId"] = self.establishment_id
-        self.client.post("/api/reservation", json=reservation)
+        self.client.post("/api/v1/reservation", json=reservation)
 
     @task
     def get_room_rack(self):
-        self.client.get(f"/api/establishments/{self.establishment_id}/rooms/")
+        self.client.get(f"/api/v1/establishments/{self.establishment_id}/rooms")
+
 

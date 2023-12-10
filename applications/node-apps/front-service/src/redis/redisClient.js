a8aad15eb6bf59ba211517d50b6746dc8d7bc6ab
Víctor H <hugohg34@gmail.com>
2023-12-10 04:20:58 +0100

    add redis autoResubscribe

--- applications/node-apps/front-service/src/redis/redisClient.js
+++ applications/node-apps/front-service/src/redis/redisClient.js
@@ -6,7 +6,8 @@ const client = new Redis({
   password: 'SUPER_SECRET_PASSWORD',
   port: 6379,
   host: "127.0.0.1",
-  db: 1
+  db: 1,
+  autoResubscribe: true
 });
 
 client.on('error', (err) => console.error('Error de Redis', err));
